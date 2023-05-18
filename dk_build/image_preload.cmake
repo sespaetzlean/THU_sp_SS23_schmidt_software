f9bbbfadf1c2c287b536291e974274604b6adf85
sespaetzlean <61940299+sespaetzlean@users.noreply.github.com>
2023-05-18 02:34:41 +0200

    build must be pushed???
    some fault, gitignore gets ignored

--- dk_build/image_preload.cmake
+++ dk_build/image_preload.cmake
@@ -1,6 +1,6 @@
 # Generated file that can be used to preload variant images
 set(BOARD "nrf52dk_nrf52832" CACHE INTERNAL "NCS child image controlled")
-set(BOARD_ROOT "f:/Github/THU_student_project_SS23_schmidt/Software/aktor_relais;D:/ncs/v2.3.0/nrf;D:/ncs/v2.3.0/zephyr" CACHE INTERNAL "NCS child image controlled")
+set(BOARD_ROOT "f:/Github/THU_student_project_SS23_schmidt/software/aktor_relais;D:/ncs/v2.3.0/nrf;D:/ncs/v2.3.0/zephyr" CACHE INTERNAL "NCS child image controlled")
 set(CACHED_CONF_FILE "F:/Github/THU_student_project_SS23_schmidt/Software/aktor_relais/prj.conf" CACHE INTERNAL "NCS child image controlled")
 set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
 set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")

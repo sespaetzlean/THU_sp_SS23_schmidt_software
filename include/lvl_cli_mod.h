a7af428066d212cd70e6c6cae7540b74147315b7
sespaetzlean <61940299+sespaetzlean@users.noreply.github.com>
2023-05-28 10:54:01 +0200

    introduced level client model

---
+++ include/lvl_cli_mod.h
@@ -0,0 +1,54 @@
+/**
+ * @file
+ * @brief Model handler
+ */
+
+#ifndef LVL_CLI_MOD_H__
+#define LVL_CLI_MOD_H__
+
+#include <zephyr/bluetooth/mesh.h>
+
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/mesh/proxy.h>
+#include <bluetooth/mesh/models.h>
+
+#include "conversion_helpers.h"
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct level_button {
+	/** Current level status of the corresponding server. */
+    uint16_t current_lvl;
+    /** Target level status of the corresponding server*/
+    uint16_t target_level;
+	/** Generic OnOff client instance for this switch. */
+	struct bt_mesh_lvl_cli client;
+	/** Number of the pin the button is connected to*/
+	int pinNumber;
+};
+
+
+/// @brief set a specific level 
+/// @param button 
+/// @param level
+/// @param transition transition struct to set delay or transition time
+/// @return error code of set message (0 if successful)
+int set_level(struct button *button, uint16_t level,
+            struct bt_mesh_model_transition *transition);
+
+
+/// @brief set a specific move speed
+/// @param button 
+/// @param delta_speed 
+/// @return error code of set message (0 if successful)
+int move_level(struct button *button, uint16_t delta_speed);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* LVL_CLI_MOD_H__ */
---
+++ src/lvl_cli_mod.c
@@ -0,0 +1,132 @@
+#include "lvl_cli_mod.h"
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(level_cli_mod, LOG_LEVEL_DBG);
+
+
+/// @brief writes the incoming status message to the level_button struct
+/// @param button struct to write to
+/// @param status status to read from
+static void level_status_writer(struct level_button *button, 
+            const struct bt_mesh_lvl_status *status)
+{
+    button->target_lvl = bt_level2input_level(status->target);
+    LOG_DBG("Created status message");
+}
+
+
+void status_handler(struct bt_mesh_lvl_cli *cli,
+            struct bt_mesh_msg_ctx *ctx,
+            const struct bt_mesh_lvl_status *status)
+{
+    struct level_button *button = CONTAINER_OF(cli, struct level_button, client);
+    level_status_writer(button, status);
+    LOG_DBG("Button %d: Received target %d ", button->target_level);
+}
+
+
+
+
+
+/// @brief check whether it is a unicast or group adress, send ack or unack command
+/// @param button level_button struct
+/// @param ctx message context
+/// @param set desired level
+/// @param rsp save response here
+/// @return error code of the set message (0 when successful)
+static int ack_unack_set_handler(struct level_button *button, 
+            struct bt_mesh_msg_ctx *ctx, 
+            struct bt_mesh_lvl_set * set, 
+            struct bt_mesh_lvl_status *rsp)
+{
+    int err;
+    /* As we can't know how many nodes are in a group, it doesn't
+	* make sense to send acknowledged messages to group addresses -
+	* we won't be able to make use of the responses anyway. (This also
+	* applies in LPN mode, since we can't expect to receive a response
+	* in appropriate time).
+	*/
+	if (bt_mesh_model_pub_is_unicast(button->client.model)) 
+	{
+		err = bt_mesh_lvl_cli_set(&button->client, ctx, set, rsp);
+		LOG_DBG("sent ACK command with level %d", set->lvl);
+	} else {
+		err = bt_mesh_lvl_cli_set_unack(&button->client, ctx, set);
+		LOG_DBG("sent UNack command with level %d", set->lvl);
+		if (!err) {
+			/* There'll be no response status for the
+			 * unacked message. Set the state immediately.
+			 */
+            struct bt_mesh_lvl_status artificial_response = {
+                .target = set->lvl;
+            }
+            level_status_writer(button, &artificial_response);
+        }
+	}
+	if (err) {
+		LOG_WRN("Level setting failed: %d\n", err);
+	}
+	return err;
+}
+
+
+
+
+
+static int ack_unack_move_handler(struct level_button *button, 
+            struct bt_mesh_msg_ctx *ctx, 
+            struct bt_mesh_lvl_move_set * set, 
+            struct bt_mesh_lvl_status *rsp)
+{
+    int err;
+    /* As we can't know how many nodes are in a group, it doesn't
+	* make sense to send acknowledged messages to group addresses -
+	* we won't be able to make use of the responses anyway. (This also
+	* applies in LPN mode, since we can't expect to receive a response
+	* in appropriate time).
+	*/
+	if (bt_mesh_model_pub_is_unicast(button->client.model)) 
+	{
+		err = bt_mesh_lvl_cli_move_set(&button->client, ctx, set, rsp);
+		LOG_DBG("sent ACK command with level %d", set->lvl);
+	} else {
+		err = bt_mesh_lvl_cli_move_set_unack(&button->client, ctx, set);
+		LOG_DBG("sent UNack command with level %d", set->lvl);
+        //TODO
+        //no artificial response here, level moves anyway
+	}
+	if (err) {
+		LOG_WRN("Level moving failed: %d\n", err);
+	}
+	return err;
+}
+
+
+
+
+
+int set_level(struct button *button, uint16_t level,
+            struct bt_mesh_model_transition *transition)
+{
+    LOG_DBG("Level Set is executed");
+    struct bt_mesh_lvl_set set = {
+        .lvl = input_level2bt_level(level),
+        .transition = transition,
+    };
+
+    return ack_unack_set_handler(button, NULL, &set, NULL);
+}
+
+
+
+
+
+int move_level(struct button *button, uint16_t delta_speed)
+{
+    LOG_DBG("Level move is executed");
+    struct bt_mesh_lvl_move_set set = {
+        .delta = delta_speed;
+    };
+
+    return ack_unack_move_handler(button, NULL, &set, NULL);
+}

48b3e578eb15962ab4b78208bea9fbee4cfe4a56
sespaetzlean <61940299+sespaetzlean@users.noreply.github.com>
2023-06-01 00:12:51 +0200

    improved comments for lvl_model

--- include/lvl_model.h
+++ include/lvl_model.h
@@ -19,23 +19,27 @@ extern "C" {
 
 #define PWM_SIZE_STEP 512		
 //by how much the pwm value should be changed each step 
-//(so changing light always uses this step size that will be called more or less often, 
+//(so changing light always uses this step size 
+//that will be called more or less often, 
 //depending how drastically light has to change)
 
 struct dimmable_ctx {
 	struct bt_mesh_lvl_srv srv;		//server instance
-	struct k_work_delayable work;	//what should be done next (will be scheduled), so what should happen with the relais
-	uint32_t remaining_time;		//remaining time until operation is finished
-	uint32_t time_period;			//how long to wait in between the steps of size: PWM_SIZE_STEP
+	struct k_work_delayable work;
+	//remaining time until operation is finished
+	uint32_t remaining_time;		
+	//how long to wait in between the steps of size: PWM_SIZE_STEP
+	uint32_t time_period;			
 	uint16_t current_lvl;
 	uint16_t target_lvl;
-	int16_t move_step; 				//how fast the light shall be changed (speed depends on time_period)
+	//how fast the light shall be changed (speed depends on time_period)
+	int16_t move_step; 				
 	void (*pwm_output)(uint16_t level);	//function pointer to execute set value
 };
 
 /// @brief set state of a dimmable element
-/// @param srv server instance	(should include current OnOff-Value)
-/// @param ctx not used
+/// @param srv server instance
+/// @param ctx 
 /// @param set new Level Status value
 /// @param rsp used to store the response
 void dimmable_set(struct bt_mesh_lvl_srv *srv,
@@ -45,16 +49,16 @@ void dimmable_set(struct bt_mesh_lvl_srv *srv,
 
 
 /// @brief get state of a dimmable element
-/// @param srv server instance	(should include current OnOff-Value)
-/// @param ctx not used
+/// @param srv server instance
+/// @param ctx 
 /// @param rsp used to store the response
 void dimmable_get(struct bt_mesh_lvl_srv *srv, struct bt_mesh_msg_ctx *ctx,
 		    struct bt_mesh_lvl_status *rsp);
 
 
 /// @brief set state of a dimmable element
-/// @param srv server instance	(should include current OnOff-Value)
-/// @param ctx not used
+/// @param srv server instance
+/// @param ctx 
 /// @param delta_set speed of the change (steps / ms)
 /// @param rsp used to store the response
 void dimmable_move_set(struct bt_mesh_lvl_srv *srv, 
--- src/lvl_model.c
+++ src/lvl_model.c
@@ -10,14 +10,15 @@ LOG_MODULE_REGISTER(lvl_model,LOG_LEVEL_DBG);
 /// @brief helper to create status msg derived from dimmable_ctx
 /// @param dimmable the struct that is used for storing in this file
 /// @param status a status instance the data should be saved to
-static void create_dimmable_status(const struct dimmable_ctx * d_ctx, struct bt_mesh_lvl_status * status)
+static void create_dimmable_status(const struct dimmable_ctx * d_ctx, 
+			struct bt_mesh_lvl_status * status)
 {
 	status->remaining_time = d_ctx->remaining_time ? d_ctx->remaining_time : 
 		k_ticks_to_ms_ceil32(k_work_delayable_remaining_get(&d_ctx->work));
 		status->target = mesh_level2struct_level(d_ctx->target_lvl);
 		status->current = mesh_level2struct_level(d_ctx->current_lvl);
-	// LOG_DBG("Created STATUS. time %d, mesh_target %d, mesh_current %d", status->remaining_time, status->target, status->current);
-	LOG_DBG("created STATUS from dimmable_ctx. rem: %d, cur: %d, tar: %d", status->remaining_time, d_ctx->current_lvl, d_ctx->target_lvl);
+	LOG_DBG("created STATUS from dimmable_ctx. rem: %d, cur: %d, tar: %d", 
+		status->remaining_time, d_ctx->current_lvl, d_ctx->target_lvl);
 }
 
 
@@ -27,26 +28,33 @@ static void create_dimmable_status(const struct dimmable_ctx * d_ctx, struct bt_
 /// @brief schedules a new SET level transition
 /// @param set the set struct that contains the target level
 /// @param ctx the dimmable stuct instance that should be updated
-static void dimmable_transition_start(const struct bt_mesh_lvl_set *set, struct dimmable_ctx *ctx)
+static void dimmable_transition_start(const struct bt_mesh_lvl_set *set, 
+			struct dimmable_ctx *ctx)
 {
-	//calculate needed parameters
-	uint32_t step_cnt = abs(set->lvl - ctx->current_lvl) / PWM_SIZE_STEP;		//how many steps (of size PWM_SIZE_STEP) are needed to reach the target value
-	uint32_t time = set->transition ? set->transition->time : 0;				//if 0 or NULL, transition time is 0
-	uint32_t delay = set->transition ? set->transition->delay : 0;				//if 0 or NULL, delay time is 0
+	/** calculate needed parameters */
+	//how many steps (of size PWM_SIZE_STEP) are needed to reach target value
+	uint32_t step_cnt = abs(set->lvl - ctx->current_lvl) / PWM_SIZE_STEP;		
+	uint32_t time = set->transition ? set->transition->time : 0;				
+	uint32_t delay = set->transition ? set->transition->delay : 0;				
 
 	//save parameters in the helper struct dimmable_ctx
 	ctx->target_lvl = struct_level2mesh_level(set->lvl);
 	ctx->time_period = (step_cnt ? time / step_cnt : 0);
 	ctx->remaining_time = time;
 
-	//to remove app bug that does not update state, also set current_lvl to target_lvl if time & delay is 0
-	//exp: this is because the app does show the current_lvl instead of the target level when delay & time is 0
+	//to remove app bug that does not update state, also set current_lvl 
+	//to target_lvl if time & delay is 0
+	//exp: this is because the app does show the current_lvl 
+	//exp: instead of the target level when delay & time is 0
 	if(!time && !delay)
 		ctx->current_lvl = ctx->target_lvl;
 
-	k_work_reschedule(&ctx->work, K_MSEC(delay));	//work will be started after delay time
-	LOG_INF("SET-trans started. Delay: %d, Trans: %d, t/step: %d, start-lvl: %d, target-lvl: %d ",delay, time, ctx->time_period , ctx->current_lvl, ctx->target_lvl);
+	//work will be started after delay time
+	k_work_reschedule(&ctx->work, K_MSEC(delay));	
+	LOG_INF("SET-trans started. Delay: %d, Trans: %d, t/step: %d, start-lvl: %d, target-lvl: %d ", 
+		delay, time, ctx->time_period , ctx->current_lvl, ctx->target_lvl);
 }
+
 
 
 
@@ -54,7 +62,8 @@ static void dimmable_transition_start(const struct bt_mesh_lvl_set *set, struct 
 /// @brief Schedules a continuous change of level (MOVE)
 /// @param set the move_set struct that contains moveStepSize and pauseTime/step
 /// @param ctx the dimmable stuct instance that should be updated
-static void dimmable_move_start(const struct bt_mesh_lvl_move_set *set, struct dimmable_ctx *ctx)
+static void dimmable_move_start(const struct bt_mesh_lvl_move_set *set, 
+			struct dimmable_ctx *ctx)
 {	
 	ctx->move_step = set->delta;
 	//check if to start or stop delta
@@ -71,26 +80,36 @@ static void dimmable_move_start(const struct bt_mesh_lvl_move_set *set, struct d
 	} else {	//ctx->move_step < 0
 		ctx->target_lvl = 0;
 	}
-	uint32_t step_cnt = abs((int32_t) ctx->target_lvl - ctx->current_lvl) / PWM_SIZE_STEP;
-	uint32_t delay = set->transition ? set->transition->delay : 0;				//if 0 or NULL, delay time is 0
+	uint32_t step_cnt = 
+		abs((int32_t) ctx->target_lvl - ctx->current_lvl) / PWM_SIZE_STEP;
+	uint32_t delay = set->transition ? set->transition->delay : 0;
 	/*for delta, not the transition form *set is used, 
 	but the theoretical one is calculated 
 	how long it would take to reach max or min level*/
-	uint32_t time = (step_cnt * set->transition->time * PWM_SIZE_STEP) / abs(ctx->move_step);			//in ms!
+	uint32_t time = (step_cnt * set->transition->time * PWM_SIZE_STEP) 
+		/ abs(ctx->move_step);
+		//in ms!
 
 	//save parameters in the helper struct dimmable_ctx
 	ctx->time_period = (step_cnt ? time / step_cnt : 0);
 	ctx->remaining_time = time;
 
 	k_work_reschedule(&ctx->work, K_MSEC(delay));
-	LOG_INF("MOVE-trans started. Delay: %d, Trans: %d, t/step: %d, #steps: %d, start-lvl: %d, move-step: %d ",delay, time, ctx->time_period, step_cnt, ctx->current_lvl, ctx->move_step);
+	LOG_INF("MOVE-trans started. Delay: %d, Trans: %d, t/step: %d, #steps: %d, start-lvl: %d, move-step: %d ", 
+		delay, 
+		time, 
+		ctx->time_period, 
+		step_cnt, 
+		ctx->current_lvl, 
+		ctx->move_step);
 }
 
 
 
 
 
-void dimmable_set(struct bt_mesh_lvl_srv *srv, struct bt_mesh_msg_ctx *ctx,
+void dimmable_set(struct bt_mesh_lvl_srv *srv, 
+			struct bt_mesh_msg_ctx *ctx,
 		    const struct bt_mesh_lvl_set *set,
 		    struct bt_mesh_lvl_status *rsp)
 {
@@ -110,7 +129,8 @@ void dimmable_set(struct bt_mesh_lvl_srv *srv, struct bt_mesh_msg_ctx *ctx,
 void dimmable_get(struct bt_mesh_lvl_srv *srv, struct bt_mesh_msg_ctx *ctx,
 		    struct bt_mesh_lvl_status *rsp)
 {
-	struct dimmable_ctx * dimmable = CONTAINER_OF(srv, struct dimmable_ctx, srv);
+	struct dimmable_ctx * dimmable = 
+		CONTAINER_OF(srv, struct dimmable_ctx, srv);
 	create_dimmable_status(dimmable, rsp);
 }
 
@@ -135,34 +155,37 @@ void dimmable_move_set(struct bt_mesh_lvl_srv *srv,
 
 
 
-
 void dimmable_work(struct k_work * work)
 {
-	struct dimmable_ctx *d_ctx = CONTAINER_OF(work, struct dimmable_ctx, work.work);
-	d_ctx->remaining_time -= d_ctx->time_period;	//subtract one time period as one step will be performed now
+	struct dimmable_ctx *d_ctx = 
+		CONTAINER_OF(work, struct dimmable_ctx, work.work);
+	//subtract one time period as one step will be performed now
+	d_ctx->remaining_time -= d_ctx->time_period;	
 
 	// * check, if transition can be regarded complete
 	//this is the case if remaining time is shorter than one time step 
-	//or when the difference between curren t& target is smaller than one PWM_SIZE_STEP
-	if((d_ctx->remaining_time <= d_ctx->time_period) || (abs(d_ctx->target_lvl - d_ctx->current_lvl) <= PWM_SIZE_STEP))
+	//or when difference between current & target is smaller than PWM_SIZE_STEP
+	if((d_ctx->remaining_time <= d_ctx->time_period) || 
+		(abs(d_ctx->target_lvl - d_ctx->current_lvl) <= PWM_SIZE_STEP))
 	{
 		//update struct
 		d_ctx->current_lvl = d_ctx->target_lvl;
 		d_ctx->remaining_time = 0;
-		//create appropriate status message
+		//create appropriate status message & publish
 		struct bt_mesh_lvl_status tempStatus;
 		create_dimmable_status(d_ctx, &tempStatus);
-		//and publish the message
 		bt_mesh_lvl_srv_pub(&d_ctx->srv, NULL, &tempStatus);
 		LOG_DBG("srv PUB, NO reschedule");
 		LOG_INF("Trans work COMPLETED.");
 	} else {	
 	// * transition not yet complete
 		if (d_ctx->target_lvl > d_ctx->current_lvl) {
-			//if target value is higher than current value, increase current value by one step
+			//if target value is higher than current value, 
+			//increase current value by one step
 			d_ctx->current_lvl += PWM_SIZE_STEP;
 		} else {
-			//if target value is lower than current value, decrease current value by one step
+			//if target value is lower than current value, 
+			//decrease current value by one step
 			d_ctx->current_lvl -= PWM_SIZE_STEP;
 		}
 		//reschedule work to be executed again after one time period
@@ -170,6 +193,5 @@ void dimmable_work(struct k_work * work)
 	}
 	//set led to new value
 	d_ctx->pwm_output(d_ctx->current_lvl);
-	//log information
 	LOG_DBG("Cur-lvl set to: %u/65535", d_ctx->current_lvl);
 }

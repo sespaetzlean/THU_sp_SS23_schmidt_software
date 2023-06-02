dbd45ba038172379d1f3f27977098e6a58b44ef9
sespaetzlean <61940299+sespaetzlean@users.noreply.github.com>
2023-06-02 23:31:38 +0200

    attention & health model: comments and blink added cooments to functions added a blinking led to attract attention

--- include/health_model.h
+++ include/health_model.h
@@ -11,17 +11,30 @@
 #include <zephyr/bluetooth/bluetooth.h>
 #include <bluetooth/mesh/models.h>
 
+#include "lc_pwm_output.h"
+
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 
+/// @brief function to execute when node shall attract attention
+/// @param mod 
 void attention_on(struct bt_mesh_model *mod);
 
+/// @brief function to stop attracting attention
+/// @param mod 
 void attention_off(struct bt_mesh_model *mod);
 
+/// @brief work struct that executes tasks to attract attention
+/// @param work 
 void attention_blink(struct k_work *work);
+
+
+/// @brief shall be called before attention is used
+/// @return 0 on success
+int attention_init();
 
 #ifdef __cplusplus
 }
--- src/health_model.c
+++ src/health_model.c
@@ -4,6 +4,15 @@
 LOG_MODULE_REGISTER(att_mod,LOG_LEVEL_DBG);
 
 
+#define INFO_LED_NODE    DT_ALIAS(info0led)
+
+#if DT_NODE_HAS_STATUS(INFO_LED_NODE, okay)
+static const struct gpio_dt_spec info_led_spec = GPIO_DT_SPEC_GET(INFO_LED_NODE, gpios);
+#else
+#error "Unsupported board: info_led devicetree alias is not defined"
+#endif
+
+
 static bool attention;
 struct k_work_delayable attention_blink_work;
 
@@ -11,9 +20,10 @@ struct k_work_delayable attention_blink_work;
 void attention_blink(struct k_work *work)
 {
 	if (attention) {
-		LOG_WRN("Attention!");
-		k_work_reschedule(&attention_blink_work, K_MSEC(1000));
+		gpio_pin_toggle_dt(&info_led_spec);
+		k_work_reschedule(&attention_blink_work, K_MSEC(300));
 	} else {
+		gpio_pin_set_dt(&info_led_spec, 0);
 		LOG_INF("Attention turned of");
 	}
 }
@@ -21,6 +31,7 @@ void attention_blink(struct k_work *work)
 void attention_on(struct bt_mesh_model *mod)
 {
 	attention = true;
+	LOG_WRN("Ready to provision!");
 	k_work_reschedule(&attention_blink_work, K_NO_WAIT);
 }
 
@@ -28,4 +39,11 @@ void attention_off(struct bt_mesh_model *mod)
 {
 	/* Will stop rescheduling blink timer */
 	attention = false;
+}
+
+int attention_init()
+{
+	int err = single_device_init(&info_led_spec);
+	k_work_init_delayable(&attention_blink_work, attention_blink);
+	return err;
 }
